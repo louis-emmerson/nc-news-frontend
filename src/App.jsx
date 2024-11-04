a83b14811f9a999b38b8b78d759dd9455e1b862c
emmo2000 <louis.emmerson2000@gmail.com>
2024-11-04 11:46:40 +0000

    added header with sliding menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 
 import './App.css'
 import { Container } from '@mui/material'
+import Header from './components/Header/Header'
 
 function App() {
 
   return (
-    <Container>
-      <p>Hi</p>
-    </Container>
+    <Header/>
+    // <Container>
+    // </Container>
   )
 }
 
