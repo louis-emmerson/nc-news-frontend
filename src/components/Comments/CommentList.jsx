7955da3d63532ef4d464ec4ac94bf11bf21438ee
emmo2000 <louis.emmerson2000@gmail.com>
2024-11-04 16:50:45 +0000

    Loading comments

--- src/components/Comments/CommentList.jsx
+++ src/components/Comments/CommentList.jsx
@@ -3,24 +3,29 @@ import CommentCard from "./CommentCard"
 import List from '@mui/material/List';
 import { getCommentsByArticleID } from "../../utils/api";
 import Error from "../Alerts/Error";
+import LinearProgress from '@mui/material/LinearProgress';
 
 
 function CommentList(props){
     const {articleID} = props
     const [comments, setComments] = useState([])
     const [isError, setIsError] = useState(false)
+    const [isLoading, setIsLoading] = useState(false)
 
     useEffect(()=>{
+      setIsLoading(true)
       setIsError(false)
       getCommentsByArticleID(articleID).then((comments)=>{
-          setComments(comments)
+        setIsLoading(false)  
+        setComments(comments)
       })
       .catch(() =>{
-        console.log("Error")
+        setIsLoading(false)  
         setIsError(true)
       })
   
     },[])
+    if(isLoading) return <LinearProgress/>
 
     if(isError) return <Error errorMsg={"There has been an error fetching comments for this article"}/>
 
